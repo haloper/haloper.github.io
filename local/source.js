d717d95e2f900c649e2291366bdcf6e7b41f55fa
shitcat <sh1tcat@gmail.com>
2015-04-30 13:10:02 +0900

    Create source.js

---
+++ local/source.js
@@ -0,0 +1,55 @@
+var source = {
+		data: [],
+		length: -1,
+		config: null,
+		init: function() {
+			return $.ajax({
+	            type:"GET",
+	            url:"local/config.json"
+			}).done(function(json) {
+				source.config = $.parseJSON(json);
+				return new Promise(function(success) {
+					success();
+				});
+			});
+		},
+		load: function(obj) {
+			var keyword = obj.keyword;
+			return $.ajax({
+				type:"GET",
+	            url:source.config.request_url
+			}).then(function(txt) {
+				var array = txt.trim().split("\n");
+				var keyword;
+				for(var  i=0;i<array.length;i++) {
+					if(array[i] && array[i].trim() != "" && !array[i].trim().startsWith("#")) {
+						if(array[i].startsWith("*")) { //Keyword
+							keyword = array[i].substring(1);
+						}
+						else if(array[i].startsWith("@")) { //Question
+							source.data[source.data.length] = {
+									keyword : keyword,
+									question : array[i].substring(1)
+							}
+						}
+						else if(array[i].startsWith("$")) { //Answer
+							source.data[source.data.length - 1].answer = array[i].substring(1);
+						}
+					}
+				}
+				source.length = source.data.length; 
+				source.next();
+				return new Promise(function(success) {
+					success();
+				});
+			});
+		},
+		next: function() {
+			var index = Math.floor(Math.random() * source.length);
+			return {
+				keyword: source.data[index].keyword,
+				question: source.data[index].question,
+				answer: source.data[index].answer
+			}
+		}
+}
