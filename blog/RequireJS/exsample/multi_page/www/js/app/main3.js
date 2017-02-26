56f4478423aa89e80f6f67e50b4c9e2d57e7fb90
hoon <sh1tcat@gmail.com>
2017-02-27 07:39:50 +0900

    requirejs complate

--- blog/RequireJS/exsample/multi_page/www/js/app/main3.js
+++ blog/RequireJS/exsample/multi_page/www/js/app/main3.js
@@ -1,11 +1,12 @@
-define(["module", "jquery", "text!res/copyright.txt", "backbone", "underscore"],
-  function (module, $, copyright) {
+define(["module", "jquery", "text!res/copyright.txt", "i18n!res/nls/message", "backbone", "underscore"],
+  function (module, $, copyright, message) {
     let userName = module.config().userName;
     $(function () {
         $('body')
             .append('<div>backbone version: ' + Backbone.VERSION + '</div>')
             .append('<div>underscore version: ' + _.VERSION + '</div>')
             .append('<div>userName ' + userName + '</div>')
-            .append('<div>' + copyright + '</div>');
+            .append('<div>' + copyright + '</div>')
+            .append('<div>' + message.red + '</div>');
     });
 });
