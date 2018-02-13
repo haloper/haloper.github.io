28381962c926daae6f042ad37f215ff4b1ed2b59
김진훈 <jinhoon.kim@nhnent.com>
2018-02-13 09:08:07 +0900

    a

--- python/data_filter.py
+++ python/data_filter.py
@@ -1,14 +1,22 @@
-from queue import Queue
+import numpy as np
+
+FEATURE_SIZE = 5
+LABEL_PAD = 1
+
+result = []
 
 
 def read_data(filename):
     file = open(filename, "r")
 
-    q = Queue(maxsize=5)
+    q = []
 
     for line in file:
         item = parse_line(line)
-        print(item)
+        q.append(item)
+        if len(q) == FEATURE_SIZE + LABEL_PAD:
+            cal_que(q)
+            q.pop(0)
 
     file.close()
 
@@ -18,6 +26,14 @@ def parse_line(line):
     for val in line.split(","):
         item.append(float(val))
     return item
+
+
+def cal_que(q):
+    que = np.array(q)
+    print(que)
+    print("=====")
+    print(que[:,0])
+    print("=====")
 
 
 read_data("../server/data/20180207")
