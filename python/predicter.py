0e16c9d2842637bd69927eea5c08d0148d5fe2d0
haloper <sh1tcat@gmail.com>
2018-02-19 22:21:52 +0900

    asdfasdf

--- python/predicter.py
+++ python/predicter.py
@@ -11,11 +11,31 @@ import data_loader
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--date', default=None, help='predict date')
+parser.add_argument('--train_steps', default=1000, type=int,
+                    help='number of training steps')
 
 
 def main(argv):
     args = parser.parse_args(argv[1:])
+
     classifier = model.get_classifier()
+
+    # Fetch the data
+    (train_x, train_y), (test_x, test_y) = data_loader.load_data()
+
+
+    # Train the Model.
+    classifier.train(
+        input_fn=lambda:data_loader.train_input_fn(train_x, train_y,
+                                                   args.batch_size),
+        steps=args.train_steps)
+
+    # Evaluate the model.
+    eval_result = classifier.evaluate(
+        input_fn=lambda:data_loader.eval_input_fn(test_x, test_y,
+                                                  args.batch_size))
+
+    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
     today = None
     if args.date == None:
@@ -23,7 +43,7 @@ def main(argv):
     else:
         today = args.date
 
-    today = "20180216"
+    today = "20180211"
 
     # print("Target file : " , today)
 
@@ -80,5 +100,5 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.FATAL)
+    tf.logging.set_verbosity(tf.logging.INFO)
     tf.app.run(main)
